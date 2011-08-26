db0063e98954c7b13e56fbafccbfac736ea8fcbf
Mathias Pettersson <mape@mape.me>
2011-08-26 19:25:28 +0200

    Fix dashboard so it deals with pjax

--- public/javascripts/dashboard.js
+++ public/javascripts/dashboard.js
@@ -1,9 +1,4 @@
 (function() {
-  // Only run on live page and frontpage
-  if (window.location.pathname.indexOf('/live') === -1 && window.location.pathname !== '/') {
-    return;
-  }
-
 	var $templates = {};
 	$('.template').each(function(index) {
 	    var $this = $(this);
@@ -14,7 +9,6 @@
   $(document).bind('end.pjax', function(e, xhr, pjax) {
     if (ws) {
       ws.disconnect();
-      ws = null;
     }
     if (pjax.url === '/live' || pjax.url === '/') {
       connect();
@@ -23,45 +17,53 @@
 
   var ws;
   function connect() {
-    ws = io.connect(null, {
-      'port': '#socketIoPort#'
-    });
-
-    ws.on('connect', function() {
-      ws.emit('join', 'irc');
-      ws.emit('join', 'twitter');
-      ws.emit('join', 'github');
-    });
-    ws.on('irc', function(irc) {
-      var $irc = $templates.irc.clone();
-      $irc.find('.msg').text(irc.message);
-      $irc.find('.name').text(irc.from+':');
-      $('.irc-dashboard ul').prepend($irc);
-      ircMessages.push($irc);
-      if (irc.length > 30) {
-        $.each(ircMessages.slice(0, ircMessages.length-30), function(index, $oldIrc) {
-          $oldIrc.remove();
-        });
-        ircMessages = ircMessages.slice(ircMessages.length-30);
-      }
-    });
-    ws.on('tweet', function(tweet) {
-      addTweet(tweet, 'search');
-    });
-    ws.on('usertweet', function(tweet) {
-      addTweet(tweet, 'user');
-    });
-    ws.on('commits', function(commits) {
-      var $gitubContainer = $('.github-commits ul').empty();
-      $.each(commits.teams, function(index, team) {
-        var $team = $templates.team.clone();
-        $team.find('.teamname').text(team.name).attr('href', '/teams/'+team.name).end()
-          .find('.count').text(team.commits+' commits').end()
-          .find('.progress').width(((team.commits/commits.max)*100)+'%').end()
-          .find('.message').text(team.message);
-        $gitubContainer.append($team);
+    if (window.location.pathname.indexOf('/live') === -1 && window.location.pathname !== '/') {
+      return;
+    }
+    if (!ws) {
+      ws = io.connect(null, {
+        'port': '#socketIoPort#'
       });
-    });
+      ws.on('connect', function() {
+        ws.emit('join', 'irc');
+        ws.emit('join', 'twitter');
+        ws.emit('join', 'github');
+      });
+      ws.on('irc', function(irc) {
+        var $irc = $templates.irc.clone();
+        $irc.find('.msg').text(irc.message);
+        $irc.find('.name').text(irc.from+':');
+        $('.irc-dashboard ul').prepend($irc);
+        ircMessages.push($irc);
+        if (irc.length > 30) {
+          $.each(ircMessages.slice(0, ircMessages.length-30), function(index, $oldIrc) {
+            $oldIrc.remove();
+          });
+          ircMessages = ircMessages.slice(ircMessages.length-30);
+        }
+      });
+      ws.on('tweet', function(tweet) {
+        addTweet(tweet, 'search');
+      });
+      ws.on('usertweet', function(tweet) {
+        addTweet(tweet, 'user');
+      });
+      ws.on('commits', function(commits) {
+        var $gitubContainer = $('.github-commits ul').empty();
+        $.each(commits.teams, function(index, team) {
+          var $team = $templates.team.clone();
+          $team.find('.teamname').text(team.name).attr('href', '/teams/'+team.name).end()
+            .find('.count').text(team.commits+' commits').end()
+            .find('.progress').width(((team.commits/commits.max)*100)+'%').end()
+            .find('.message').text(team.message);
+          $gitubContainer.append($team);
+        });
+      });
+    }
+    // For some reason this fixes Uncaught Error: INVALID_STATE_ERR: DOM Exception 11
+    setTimeout(function() {
+      ws.socket.connect();
+    }, 0);
   }
   connect();
   // Irc
