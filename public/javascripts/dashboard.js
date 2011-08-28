fcff248950c126529fd89fa3486f45dc6b91da52
Guillermo Rauch <rauchg@gmail.com>
2011-08-28 13:18:02 -0700

    Rounding seconds

--- public/javascripts/dashboard.js
+++ public/javascripts/dashboard.js
@@ -111,7 +111,7 @@
       return;
         
     return day_diff == 0 && (
-        diff < 60 && diff + ' seconds ago' ||
+        diff < 60 && Math.round(diff) + ' seconds ago' ||
         diff < 120 && "1 minute ago" ||
         diff < 3600 && Math.floor( diff / 60 ) + " minutes ago" ||
         diff < 7200 && "1 hour ago" ||
